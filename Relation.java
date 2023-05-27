f9aebd77d03d9b6efa657119bd750c19800692bd
raman <raman31293@gmail.com>
2023-05-27 12:19:13 +0530

    my first repo

---
+++ Relation.java
@@ -0,0 +1,30 @@
+public class Relation {
+    public static void main(String[] args) {
+        int num1 = 10 ; int num2 = 20;
+        if (num1 == num2){
+            System.out.println("Equal : True");
+        }
+        else{
+            System.out.println("Equale : False");
+        }
+        if (num1 != num2){
+            System.out.println("Not Equal : True");
+        }
+        else{
+            System.out.println("Not Equale : False");
+        }
+        if (num1 >= num2){
+            System.out.println("Num1 is greater than Num2 : True");
+        }
+        else{
+            System.out.println("Num1 is greater than Num2 : False");
+        }
+        if (num1 <= num2){
+            System.out.println("Num2 is greater than Num1 : True");
+        }
+        else{
+            System.out.println("Num2 is greater than Num1 : False");
+        }
+        
+    }      
+}
---
+++ assignment.java
@@ -0,0 +1,20 @@
+public class assignment {
+    public static void main(String[] args) {
+        int num1 = 10;
+        num1 += 2;
+        System.out.print("After Addition :");
+        System.out.println(num1);
+        num1 -= 4;
+        System.out.print("After subsracttion:");
+        System.out.println(num1);
+        num1 *= 7;
+        System.out.print("After multipolicaion: ");
+        System.out.println(num1);
+        num1 /= 4;
+        System.out.print("After division :");
+        System.out.println(num1);
+        num1 %= 3;
+        System.out.print("After module: ");
+        System.out.println(num1);
+    }      
+}
